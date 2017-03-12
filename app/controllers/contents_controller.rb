413b2f8b3cb112fc9bba1b6f7f0e361fb1d80e34
liliabula <liliabula@gmail.com>
2017-03-12 14:31:24 +0000

    autorizzazioni e associazioni per user e content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,7 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_user, only: [:edit, :update, :destroy]
   # GET /contents
   # GET /contents.json
   def index
@@ -14,7 +15,7 @@ class ContentsController < ApplicationController
 
   # GET /contents/new
   def new
-    @content = Content.new
+    @content = current_user.contents.build
   end
 
   # GET /contents/1/edit
@@ -24,7 +25,7 @@ class ContentsController < ApplicationController
   # POST /contents
   # POST /contents.json
   def create
-    @content = Content.new(content_params)
+    @content = current_user.contents.build(content_params)
 
     respond_to do |format|
       if @content.save
@@ -71,4 +72,10 @@ class ContentsController < ApplicationController
     def content_params
       params.require(:content).permit(:title, :description, :text, :price)
     end
+end
+
+def check_user
+  if current_user != @content.user
+    redirect_to root_url, alert: "Sorry, you don't have the access to this page."
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312001545) do
+ActiveRecord::Schema.define(version: 20170312122908) do
 
   create_table "contents", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170312001545) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
